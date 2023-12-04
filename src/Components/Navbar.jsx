f0acf0746dc04966b7df6eb65537f6f620c75ae1
Fozzyack <fsundra@gmail.com>
2023-12-04 15:32:09 +0800

    we have a logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import NavLink from './NavLink';
 import { Link } from 'react-router-dom';
 import NavMenuMobile from './NavMenuMobile';
-
+import logo from '../assets/noice.png'
 const Navbar = () => {
 
     const [openMenu, setOpenMenu] = useState(false);
@@ -35,9 +35,12 @@ const Navbar = () => {
     ]
     return (
         <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-95'>
-            <div className='flex flex-wrap items-center justify-between mx-auto  px-4'>
-                <Link className='text-white text-sm md:text-4xl font-semibold'>[ PROFESSIONAL PROCRASTINATOR ]</Link>
-                <div className='mobile-menu block md:hidden px-3 py-3'>
+            <div className='flex flex-wrap items-center justify-between mx-auto  px-4 py-2'>
+                <button className='text-white text-sm md:text-4xl font-semibold flex items-center'>
+                    <img src={logo} alt='logo' className='w-[90px]'/>
+                    <p className='hidden lg:block'> [ PROFESSIONAL PROCRASTINATOR ]</p>
+                </button>
+                <div className='mobile-menu block lg:hidden px-3 py-3'>
                     {
                         openMenu ? (
                             <button onClick={() => setClose()} className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
@@ -54,7 +57,7 @@ const Navbar = () => {
                         )
                     }
                 </div>
-                <div className='menu hidden md:block md: w-auto'>
+                <div className='menu hidden lg:block md: w-auto'>
 
                     <ul className='flex p-4 md:mt-0 sm:flex-row md:space-x-8 mt-10'>
                         {links.map((link, index) => (
