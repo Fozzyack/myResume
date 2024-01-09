98879f25d40ef84eef257b5d2b9e1f5a26dea482
Fozzyack <fsundra@gmail.com>
2024-01-10 01:08:45 +0800

    Removing Testimonials from Navbar Removing the testimonials as there is no longer a section for them Once aquiring I may add them back in the future

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -31,13 +31,6 @@ const Navbar = () => {
             </svg>
         },
         {
-            href: 'testimonials',
-            title: 'Testimonials',
-            image: <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 17V2a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M5 15V1m8 18v-4"/>
-          </svg>
-        },
-        {
             href: 'contacts',
             title: 'Contact',
             image: <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 18">
